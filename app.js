92e6e2a1e3b6b9e3df8b5c9c201febd5080011fd
Veronica Robinson <robinson.ver18@gmail.com>
2025-08-25 16:35:51 -0600

    Cheeses

--- app.js
+++ app.js
@@ -40,14 +40,16 @@ let automaticUpgrades = [
 
 
 // NOTE this should only be called when the img is clicked
+
+// NOTE forEach - does something with each item in the array 
+// NOTE find - does something with one item from an array
 function mine() {
 
     cheese += 1;
     clickUpgrades.forEach((click) => {
         if (click.quantity >= 1) {
-            //FIXME click.quantity TIMES click.bonus
-            cheese += click.bonus
-
+            // click.quantity * click.bonus
+            cheese += click.quantity * click.bonus
         }
 
     })
@@ -55,110 +57,110 @@ function mine() {
     drawCheese()
     drawAutoUpgrades()
     drawButtonUpgrades()
+    // drawCPC()
+    // drawCPI()
 
 }
 
 
-function buyClickUpgrade() {
+function buyClickUpgrade(cName) {
 
     // TODO use a FIND to get the correct upgrade out of the array (reference getAnimal)
     // TODO we don't need the double for loop, but all of the logic in your if statement is correct
-    for (let i = 0; i < clickUpgrades.length; i++) {
 
-        // let click = clickUpgrades[i]
-        clickUpgrades.forEach((click) => {
-            if (cheese >= click.price) {
-                click.quantity += 1;
-                cheese -= click.price;
-                click.price += click.price;
-            }
-        })
+    let selectedClick = clickUpgrades.find((click) => click.name == cName)
+    if (cheese >= selectedClick.price) {
+        selectedClick.quantity += 1;
+        cheese -= selectedClick.price;
+        selectedClick.price += selectedClick.price;
     }
+    console.log('selected', selectedClick)
+
 
     drawCheese()
     drawButtonUpgrades()
+    drawCPC()
 
 }
 
 
 
 
-function buyAutoMaticUpgrade() {
+function buyAutoMaticUpgrade(aName) {
 
     // TODO reference buyClickUpgrade for refactoring this
+    let selectedAutomatic = automaticUpgrades.find((a) => a.name == aName)
 
-    automaticUpgrades.forEach((a) => {
-        if (cheese >= a.price) {
-            a.quantity += 1;
-            cheese -= a.price;
-            a.price += a.price;
-            a.bonus += a.bonus;
+    if (cheese >= selectedAutomatic.price) {
+        selectedAutomatic.quantity += 1;
+        cheese -= selectedAutomatic.price;
+        selectedAutomatic.price += selectedAutomatic.price;
 
-        }
 
-    })
+    }
 
 
     drawCheese()
     drawAutoUpgrades()
-    // TODO call drawcpc
-
+    drawCPI()
 }
 
-function buyAutoMaticUpgrade() {
 
-    let a = automaticUpgrades[1];
+
+function collectAuto() {
+    // let autoPayout = 0
     automaticUpgrades.forEach((a) => {
-        if (cheese >= a.price) {
-            a.quantity += 1;
-            cheese -= a.price;
-            a.price += a.price;
-            a.bonus += a.bonus
+        if (a.quantity >= 1) {
+            // a.bonus * a.quantity;
+            cheese += a.bonus * a.quantity
 
         }
 
     })
-
     drawCheese()
     drawAutoUpgrades()
-    // TODO call drawcpi
-
-
+    // drawCPI()
 }
 
-// function collectAuto() {
-//     let autoPayout = 0
-//     automaticUpgrades.forEach((a) => {
-// TODO a.bonus TIMES a.quantity
-//         cheese += a.bonus
-
-//     })
-//     drawAutoUpgrades()
-// }
-
+setInterval(collectAuto, 1000 * 3)
 
 // TODO you need to call your collect auto function here instead of buying
-setInterval(buyAutoMaticUpgrade, 1000 * 3)
 
 
 // VISUALIZERS -------------------------------------------------
 
 let cheeseElm = document.getElementById("cheese")
-let clickElm = document.getElementById(`clicks`)
-
+let clickElm = document.getElementById("clicks")
+let aElm = document.getElementById("automatic")
 
 function drawCheese() {
-    cheeseElm.innerHTML = `${cheese}`
-    console.log('🧀', cheese)
-}
 
+    cheeseElm.innerHTML = `<b> Cheese: ${cheese} </b>`
+
+}
 
 function drawCPC() {
-    let cpc = 0
+    let cpc = 1;
+    console.log('hi')
+    clickUpgrades.forEach((c) => {
+        if (c.quantity >= 1) {
+            cpc += c.bonus * c.quantity
+        }
+    })
+
     // TODO looop over your clickUpgrades array, and add each upgrade's bonus * quantity to cpc
-    clickElm.innerHTML = `Clicks: ${cpc}`
+    clickElm.innerHTML = `<b>Clicks: ${cpc}</b>`
 }
 
+function drawCPI() {
+    let cpi = 0;
+    automaticUpgrades.forEach((a) => {
+        if (a.quantity >= 1) {
+            cpi += a.bonus * a.quantity
+        }
+    })
+    aElm.innerHTML = `<b>Automatic: ${cpi}</b>`
+}
 // TODO write a draw cpi function, use drawCPC as a reference
 
 function drawButtonUpgrades() {
@@ -174,9 +176,9 @@ function drawButtonUpgrades() {
 
 function drawAutoUpgrades() {
     automaticUpgrades.forEach((u) => {
-        let buttonUpgradeElm = document.getElementById(`${u.name}-button`)
+        let buttonAutoUpgradeElm = document.getElementById(`${u.name}-button`)
         let upgradeStatElm = document.getElementById(`${u.name}-stats`)
-        buttonUpgradeElm.innerHTML = `<h3>${u.name} 🧀${u.price}</h3> bonus: ${u.bonus}`
+        buttonAutoUpgradeElm.innerHTML = `<h3>${u.name} 🧀${u.price}</h3> bonus: ${u.bonus}`
         upgradeStatElm.innerHTML = `<span class="text-capitalize">${u.name}: ${u.quantity}</span>`
     })
 }
@@ -189,10 +191,6 @@ function drawAutoUpgrades() {
 drawCheese()
 drawButtonUpgrades()
 drawAutoUpgrades()
-// buyClickUpgrade()
-// buyDrillUpgrade()
-// buyClickUpgrade0()
-// buyClickUpgrade1()
-// buyAutoMaticUpgrade()
-// buyAutoMaticUpgrade()
+
 drawCPC()
+drawCPI()
